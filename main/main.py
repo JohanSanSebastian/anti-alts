e9af5e9d94fad4053c26673dd9bc08b39c54f091
nsde <neostyxde@gmail.com>
2021-01-27 20:19:55 +0100

    It's working as expected, but planning to add more

--- main/main.py
+++ main/main.py
@@ -4,6 +4,7 @@ try:
     # STANDARD LIBARY
     import os
     import sys
+    import time
 
     # PIP
     import yaml
@@ -13,45 +14,73 @@ except:
     print("Couldn't load all the libaries. Please install the libaries listed in requirements.txt.")
     sys.exit(0)
 
-# Set token
-try:
-    token = ""
-
-except:
-    print(
-        "Please type in a valid bot token. The token can be found at the Discord Developer Portal at 'applications/<application id>/bot'.")
-    print("Type in a bot token or press enter to skip.")
-    set_token = input()
-    if set_token != "":
-        os.environ["scdtoken"] = set_token
-        print("Updated the 'scdtoken' to '" + os.environ["scdtoken"] + "'.")
-    sys.exit(0)
-
 cwd = os.getcwd()
 
-client = commands.Bot(command_prefix='!')
+# Set token
+try:
+    token = open(cwd + "\\token.txt").read()
 
+except:
+    print("""Please type in a valid bot token.
+    The token can be found at the Discord Developer Portal at 'applications/<application id>/bot'.""")
+    sys.exit(0)
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix=".", intents=intents)
 
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+    print(f"Logged in as {client}")
+
+@client.event
+async def on_member_join(member):
+    print("join")
+    if time.time() - member.created_at.timestamp() < 2592000: # 2592000
+        print("time")
+        channel = client.get_channel(799605130222895114)
+        print("channel")
+        await channel.send(f"> :no_entry: {member.mention} Alt account detected")
+        await member.send("""
+        :no_entry: Your account has been blocked on blockabl.
+
+        > **Blocked by:** Bot (blockabl AntiAlt)
+
+        > **Reason:** we believe that this is an alt account.
+
+        > **What does that mean?** You don't have full access to
+        > certain features of both the Discord server and the
+        > Minecraft server, e.g. writing in Channels, at least on
+        > this account.
+
+        > **False-positive! I'm innocent!** If this is NOT an alt account,
+        > we apologize for this. Contact us and don't worry.
+
+        > **When do I get unblocked?** If this is in fact an alt account,
+        >  don't be suprised if we even KICK or BAN you.
+
+        For further information, contact us via `#support`.
+            """)
+    else:
+        pass
 
 
-@client.command(name='hello')
+@client.command(name="ping")
 async def hello(ctx):
-    await ctx.send('Hello')
+    await ctx.send("Ping!")
 
 
-@client.command(name='say')
+@client.command(name="say")
 async def say(ctx, *args):
-    var = ''
+    var = ""
     for s in args:
-        var += f'{s} '
+        var += f"{s} "
     await ctx.send(var)
 
 
-@client.command(name='user')
-async def user(ctx):
+@client.command(name="check")
+async def user(ctx, *args):
     user = ctx.message.author
     mention = ctx.message.author.mention
     id = ctx.message.author.id
@@ -60,17 +89,15 @@ async def user(ctx):
     await ctx.send(id)
 
 
-@client.command(name='stop')
+@client.command(name="kill")
 @commands.has_permissions(administrator=True)
 async def stop_bot(ctx):
-    """Stoppt den Bot (Admin)"""
-
-    await ctx.send('Bot stoppt...')
+    await ctx.send("Terminating the bot...")
     await client.close()
-
+    sys.exit(0)
 
 try:
     client.run(token)
-
 except:
     print("ERROR: Unable to run the client. Did you input a invalid token?")
+    sys.exit(0)
